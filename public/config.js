8bc4121e627c3c9df5962a578baeb91ee3ad8f8e
Rena-O23 <100457700+InariSaika@users.noreply.github.com>
2026-05-28 10:42:11 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -3,6 +3,6 @@
 // 注意：这里只放 anon key（只读权限），不要放 service key
 
 window.GALLERY_CONFIG = {
-  supabaseUrl: 'https://lnuubpxeotvvqrcezvpe.supabase.co/rest/v1/',
+  supabaseUrl: 'https://lnuubpxeotvvqrcezvpe.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxudXVicHhlb3R2dnFyY2V6dnBlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5MTk0MDQsImV4cCI6MjA5NTQ5NTQwNH0.Nqk6FE2twPD7jjMHaW2qgFchs31YO6a-QdXsmFdVR6c',
 };
